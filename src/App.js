7d61653709db17095e9a101366770c65f27f94c8
Seonghwan <tgb023620@gmail.com>
2019-08-21 10:02:24 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 에러를 감지하고 판단하는 class
+// component를 input and output으로 사용
+const BoundaryHOC = ProtectedComponent => class Boundary extends Component{
+  state = {
+    hasError: false
+  };
+  componentDidCatch = () =>{
+    this.setState({
+      hasError: true
+    });
+  };
+  render(){
+    const {hasError} = this.state;
+    if(hasError){
+      return <ErrorFallback/>;
+    }else{
+      return <ProtectedComponent />
+    }
+  }
+}
+
+
+class ReturnTypes extends Component{
+  render(){
+    return (
+      <Fragment>
+        <header></header>
+        <div></div>
+        <footer></footer>
+      </Fragment>
+    )
+  }
+}
+
+class ErrorMaker extends Component{
+  state = {
+    friends: ["jisu", "flynn", "daal", "kneeprayer"]
+  }
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      });
+    }, 2000);
+  };
+  render(){
+    const {friends} = this.state;
+    return friends.map(friend => `${friend} `);
+  }
+}
+
+const ErrorFallback = () => "Sorry something went wrong";
+const EErrorMaker = BoundaryHOC(ErrorMaker);
+
+class App extends Component {
+  state = {
+    hasError: false
+  }
+  componentDidCatch = (error, info) => {
+    console.log(`catched ${error} the info I have is ${JSON.stringify(info)}`);
+    this.setState({hasError: true})
+  }
+  render(){
+    const {hasError} = this.state;
+    return (
+      <div className="App">
+        <h1>Hello everyone</h1>
+        {hasError ? <ErrorFallback/> : <ErrorMaker/>}
+        <EErrorMaker/>
+      </div>
+    );
+  }
 }
 
 export default App;
